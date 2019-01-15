c6bea75ed9ce9f18dd8a8108281fbe16d6b2704c
Henry Beberman <henry.beberman@microsoft.com>
2019-01-15 11:33:03 -0800

    Update build/tools/BuildImage.bat
    Co-Authored-By: neilsh-msft <neilsh@microsoft.com>

--- build/tools/BuildImage.bat
+++ build/tools/BuildImage.bat
@@ -92,7 +92,7 @@ set PKG_VER=1.0.0.0
 if not exist %BSPPKG_DIR% ( mkdir %BSPPKG_DIR% )
 
 if not exist %REPO_BUILD_ROOT%\..\..\b\%PLATFORM%\%Configuration% (dir /S /B %REPO_BUILD_ROOT%\solution\iMXPlatform\Build\%PLATFORM%\%Configuration%\*.cab > filelist.txt)
-if exist %REPO_BUILD_ROOT%\..\..\b\%PLATFORM%\%Configuration% (dir /S /B %REPO_BUILD_ROOT%\..\..\b\%PLATFORM%\%Configuration%\*.cab > filelist.txt)
+if exist %REPO_BUILD_ROOT%\..\..\b\%Configuration%\%PLATFORM% (dir /S /B %REPO_BUILD_ROOT%\..\..\b\%Configuration%\%PLATFORM%\*.cab > filelist.txt)
 
 REM Append the %BOARD_NAME%Package folder to the end of the scrape list so the correct SV.PlatExtensions.UpdateOS.cab is in bspcabs
 if not exist %REPO_BUILD_ROOT%\..\..\b\%PLATFORM%\%Configuration% (dir /S /B %REPO_BUILD_ROOT%\solution\iMXPlatform\Build\%PLATFORM%\%Configuration%\%BOARD_NAME%Package\*.cab >> filelist.txt)
