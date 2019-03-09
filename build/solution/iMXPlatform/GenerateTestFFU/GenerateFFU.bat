3e34994092c9706cd0a5c67f899a556c31002563
David Vescovi <dvescovi@tampabay.rr.com>
2019-03-09 01:30:42 -0500

    add iMX8 Mini to generateFFU batch file (#43)
    * add mx8 mini to batch file
    
    * remove tab

--- build/solution/iMXPlatform/GenerateTestFFU/GenerateFFU.bat
+++ build/solution/iMXPlatform/GenerateTestFFU/GenerateFFU.bat
@@ -59,4 +59,8 @@ if "%PLATFORM%"=="ARM64" (
     cd /d %BATCH_HOME%
     echo "Building NXPEVK_iMX8M_4GB FFU"
     call BuildImage NXPEVK_iMX8M_4GB NXPEVK_iMX8M_4GB_TestOEMInput.xml
+ 
+    REM cd /d %BATCH_HOME%
+    REM echo "Building NXPEVK_iMX8M_Mini_2GB FFU"
+    REM call BuildImage NXPEVK_iMX8M_Mini_2GB NXPEVK_iMX8M_Mini_2GB_TestOEMInput.xml
 )
